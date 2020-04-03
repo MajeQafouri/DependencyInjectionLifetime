652b630d69e3b25c835b1c5092316eab754588b0
MehadiFalamazi <falamarzijahromi.mm@gmail.com>
2020-04-03 18:35:17 +0430

    Added utility of transient and scoped services in a singleton service

---
+++ ServiceScope/Services/Factories/ScopeServiceFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceScope.Models;
+
+namespace ServiceScope.Services.Factories
+{
+    /// <summary>
+    /// by https://www.linkedin.com/in/falamarzijahromi-mm
+    /// </summary>
+    public interface IScopeServiceFactory
+    {
+        IScopeService GetScopeService();
+    }
+
+    public class ScopeServiceFactory : IScopeServiceFactory
+    {
+        public IServiceProvider ServiceProvider { get; }
+
+        public ScopeServiceFactory(IServiceProvider serviceProvider)
+            =>
+            ServiceProvider = serviceProvider;
+
+        public IScopeService GetScopeService()
+        {
+            var httpContext = ServiceProvider.GetService<IHttpContextAccessor>().HttpContext;
+
+            return httpContext.RequestServices.GetService<IScopeService>();
+        }
+    }
+}
---
+++ ServiceScope/Services/Factories/TransientServiceFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceScope.Models;
+using System;
+
+namespace ServiceScope.Services.Factories
+{
+    /// <summary>
+    /// by https://www.linkedin.com/in/falamarzijahromi-mm
+    /// </summary>
+    public class TransientServiceFactory : ITransientServiceFactory
+    {
+        public IServiceProvider ServiceProvider { get; }
+
+        public TransientServiceFactory(IServiceProvider serviceProvider)
+            =>
+            ServiceProvider = serviceProvider;
+        
+
+        public ITransientService GetTransientService() 
+            =>
+            ServiceProvider.GetService<ITransientService>();
+    }
+
+    public interface ITransientServiceFactory
+    {
+        ITransientService GetTransientService();
+    }
+}
